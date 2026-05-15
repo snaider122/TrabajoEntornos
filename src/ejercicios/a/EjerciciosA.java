811bd079f2e30250593b113d6f36a36aad598d47
carlo <carlo@LAPTOP-CVI11L5C>
2026-05-15 10:33:17 +0200

    1

---
+++ src/ejercicios/a/EjerciciosA.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.a;
+
+
+
+public class EjerciciosA {
+
+    
+    public static void main(String[] args) {
+        
+        
+        for(int x=0; x<1000000000; x++){
+        
+         
+              System.out.println();    
+              
+              
+              
+              
+              
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+    
+    
+}
